13e874c4b4474f1b87e73a17a07eed0546191124
Viering <viering@users.noreply.github.com>
2015-07-02 12:18:28 +0200

    impled sampleOn

--- test/behavior/lib/label.rb
+++ test/behavior/lib/label.rb
@@ -1,3 +1,4 @@
+require 'singleton'
 class Label
 	 @x
 	 @y
@@ -21,5 +22,23 @@ class Label
 
 	 def set_y (y)
 	 	@y = y
+	 end
+end
+class NullLable
+	 include Singleton
+
+
+	 def get_x
+	 	0
+	 end
+
+	 def get_y
+	 	0
+	 end
+
+	 def set_x(x)
+	 end
+
+	 def set_y (y)
 	 end
 end
--- test/behavior/lib/test.rb
+++ test/behavior/lib/test.rb
@@ -1,8 +1,10 @@
 require_relative 'mouse'
+require_relative 'label'
 
 extend BehaviorCore
 
 mouse = Mouse.new
+lbl = Label.new(0,0)
 
 
 #lbl = Lable.new(0,0)
@@ -12,32 +14,29 @@ pos = mouse.asBehavior.map{ |mice| [mice.x, mice.y] }
 
 mouseDown = mouse.asBehavior.map{ |mice| mice.status}
 
+def trag(element, mouseDownB, posB)
+	elUnderMouse = posB.map { |pos |
+		puts "#{pos[0]} == #{element.get_x} && #{pos[1]} == #{element.get_y}"
+		if( pos[0] == element.get_x && pos[1] == element.get_y)
+			element
+		else
+			NullLable.instance
+		end
+	}
+	return (elUnderMouse.sampleOn(mouseDownB)).map(posB) { |el, pos| [el,pos[0],pos[1]]  }
+end
 
-pos.onChange{ |x| puts x}
+
+tragObj = trag(lbl,mouseDown, pos)
 
 
+tragObj.onChange{ |x| puts x}
 
-#for i in 1 .. 100
-#	mouse.set_x(mouse.value.x + 1)
-#	mouse.set_y(mouse.value.y + 0.3)
-#end
 
-    s1 = source(1)
-    sb = source(true)
+mouse.mouse_down
+for i in 1 .. 10
 
-    sample = s1.sampleOn(sb)
-    puts sample.now
-    s1.emit(2)
-    puts sample.now
-    s1.emit(3)
-    puts sample.now
-    sb.emit(false)
-    puts sample.now
-    s1.emit(2)
-    puts sample.now
-    s1.emit(6)
-    puts sample.now
-    sb.emit(true)
-    puts sample.now
-    s1.emit(7)
-    puts sample.now
+	mouse.set_x(mouse.value.x + 1)
+	mouse.set_y(mouse.value.y + 0.3)
+end
+
